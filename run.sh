98c9981060f569839346f417526ac649de0794f5
dannftk <dannftk@yandex.ru>
2013-10-09 00:24:23 +0400

    changed run.sh in accordance with chages with the structure of the project and new logic of making project

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 
-bin/plcmp prog/${1}.pli && 
-bin/asmcmp prog/${1}.ass && 
+plcmp/bin/plcmp prog/${1}.pli && 
+asmcmp/bin/asmcmp prog/${1}.ass && 
 printf "prog/${1}.tex" > prog/${1}.mod && 
-bin/absload prog/${1}.mod
+absload/bin/absload prog/${1}.mod
