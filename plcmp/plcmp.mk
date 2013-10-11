16c708ade6e94d63c5b95920b3fba2ae8b1887b6
dannftk <dannftk@yandex.ru>
2013-10-12 00:08:27 +0400

    move include of deps of plcmp below

--- plcmp/plcmp.mk
+++ plcmp/plcmp.mk
@@ -31,8 +31,6 @@ PLCMP_OBJS := $(patsubst %.c, $(PLCMP_OBJ_DIR)/%.o, $(PLCMP_SRCS_NOTDIR))
 # PL COMPILER DEPENDENCIES
 PLCMP_DEPS := $(patsubst %.c, $(PLCMP_DEP_DIR)/%.d, $(PLCMP_SRCS_NOTDIR))
 
--include $(PLCMP_DEPS)
-
 build: $(PLCMP_OBJS)
 	$(CC) $(PLCMP_OBJS) -o $(PLCMP_BIN)
 clean:
@@ -42,5 +40,7 @@ $(PLCMP_OBJ_DIR)/%.o: $(PLCMP_SRC_DIR)/%.c
 	$(CC) -c $< $(CC_FLAGS) $(PLCMP_INCLUDES) -MM -MT $@ -MF \
 $(addprefix $(PLCMP_DEP_DIR)/, $(patsubst $(PLCMP_OBJ_DIR)/%.o, %.d, $@))
 	$(CC) -c $< $(CC_FLAGS) $(PLCMP_INCLUDES) -o $@
+
+-include $(PLCMP_DEPS)
 
 .PHONY: build clean
